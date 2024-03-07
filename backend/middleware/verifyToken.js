93dbae1320ef4735ce0a004a395eae31fe49cbbc
ngdtai2k2 <ngdtai2k2@gmail.com>
2024-03-07 16:50:56 +0700

    add file middleware

---
+++ backend/middleware/verifyToken.js
@@ -0,0 +1,44 @@
+const jwt = require("jsonwebtoken");
+
+const verifyMiddleware = {
+  token: (req, res, next) => {
+    const token = req.headers.token;
+    if (!token) {
+      return res.status(401).json({
+        error: "No token provided!",
+      });
+    }
+    const accessToken = token.split(" ")[1];
+    jwt.verify(accessToken, process.env.JWT_ACCESS_KEY, (err, user) => {
+      if (err) {
+        return res.status(401).json({
+          error: "Invalid token!!!",
+        });
+      }
+      req.user = user;
+      next();
+    });
+  },
+
+  verifyTokenAndUserAuthorization: (req, res, next) => {
+    verifyMiddleware.token(req, res, () => {
+      if (req.user._id == req.params.id || req.user.isAdmin) {
+        next();
+      } else {
+        return res.status(403).json({ message: "You're not allowed!" });
+      }
+    });
+  },
+
+  tokenAndAdminAuth: (req, res, next) => {
+    verifyMiddleware.token(req, res, () => {
+      if (req.user.isAdmin) {
+        next();
+      } else {
+        return res.status(403).json({ message: "You're not allowed!" });
+      }
+    });
+  }
+};
+
+module.exports = verifyMiddleware;
