b2366ca885288dfa825519241cd3e50c8bb14a90
ngdtai2k2 <ngdtai2k2@gmail.com>
2024-03-08 19:01:17 +0700

    Remove styled hidden input

--- frontend/src/pages/settings/styles.js
+++ frontend/src/pages/settings/styles.js
@@ -1,25 +1,12 @@
-import { Tab, Typography } from '@mui/material';
-import { styled } from '@mui/material/styles';
+import { Tab, Typography } from "@mui/material";
+import { styled } from "@mui/material/styles";
 
 export const TypographyButtonTab = styled(Typography)(() => ({
-  display: 'flex',
-  alignItems: 'center',
+  display: "flex",
+  alignItems: "center",
 }));
 
 export const ButtonTab = styled(Tab)(() => ({
-  display: 'flex',
-  alignItems: 'flex-start',
+  display: "flex",
+  alignItems: "flex-start",
 }));
-
-export const VisuallyHiddenInput = styled('input')({
-  clip: 'rect(0 0 0 0)',
-  clipPath: 'inset(50%)',
-  height: 1,
-  overflow: 'hidden',
-  position: 'absolute',
-  bottom: 0,
-  left: 0,
-  whiteSpace: 'nowrap',
-  width: 1,
-  cursor: 'pointer',
-});
