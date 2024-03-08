90792f482d64aae43f5c1ef19d857fa0a22bd778
ngdtai2k2 <ngdtai2k2@gmail.com>
2024-03-08 18:58:28 +0700

    Refactor code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,63 +4,22 @@ import BlockedRoute from './routes/blockedRoute';
 import Home from './pages/home';
 import Login from './pages/login';
 import NavigationBar from './components/NavigationBar';
+import NotFound from './components/NotFound';
 import Profile from './pages/profile';
 import Register from './pages/register';
 import Settings from './pages/settings';
-import NotFound from './components/NotFound';
+import CreatePost from './pages/createPost';
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route
-          path="/"
-          exact
-          element={
-            <>
-              <NavigationBar />
-              <Home />
-            </>
-          }
-        />
-        <Route
-          path="/profile/:id"
-          exact
-          element={
-            <>
-              <NavigationBar />
-              <Profile />
-            </>
-          }
-        />
-        <Route
-          path="/settings"
-          exact
-          element={
-            <BlockedRoute>
-              <NavigationBar />
-              <Settings />
-            </BlockedRoute>
-          }
-        />
-        <Route
-          path="/login"
-          exact
-          element={
-            <BlockedRoute blocked="true">
-              <Login />
-            </BlockedRoute>
-          }
-        />
-        <Route
-          path="/register"
-          exact
-          element={
-            <BlockedRoute blocked="true">
-              <Register />
-            </BlockedRoute>
-          }
-        />
+        <Route path="/" exact element={<><NavigationBar /><Home /></>}/>
+        <Route path="/profile/:id" exact element={<><NavigationBar /><Profile /></>}/>
+        <Route path="/settings" exact element={<BlockedRoute><NavigationBar /><Settings /></BlockedRoute>}/>
+        <Route path="/login" exact element={<BlockedRoute blocked="true"><Login /></BlockedRoute>}/>
+        <Route path="/register" exact element={<BlockedRoute blocked="true"><Register /></BlockedRoute>}/>
+        <Route path="/create/post" exact element={<BlockedRoute><NavigationBar /><CreatePost /></BlockedRoute>}/>
         <Route path="*" element={<NotFound />} />
       </Routes>
     </Router>
