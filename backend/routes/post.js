38ca4bf1bc181801308c5fbe5f93403b4fddf551
ngdtai2k2 <ngdtai2k2@gmail.com>
2024-03-08 16:37:46 +0700

    Update create post route

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -2,9 +2,15 @@ const router = require("express").Router();
 
 const verifyMiddleware = require("../middleware/verifyToken");
 const postController = require("../controllers/postController");
+const storage = require("../configs/multer");
 
 router.get("/", postController.getAllPost);
 router.get("/:id", postController.getPostWithMediaById);
-router.post("/create", verifyMiddleware.token, postController.createPost);
+router.post(
+  "/create",
+  verifyMiddleware.token,
+  storage.single("file"),
+  postController.createPost
+);
 
 module.exports = router;
