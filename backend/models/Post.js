ce53853da219d30e4707455f6743f768682c43d9
ngdtai2k2 <ngdtai2k2@gmail.com>
2024-03-09 09:54:34 +0700

    Change maxlengt content field in post collection

--- backend/models/Post.js
+++ backend/models/Post.js
@@ -11,7 +11,6 @@ const postSchema = new mongoose.Schema(
     content: {
       type: String,
       required: true,
-      maxlenght: 280,
     },
     category: {
       type: mongoose.Schema.Types.ObjectId,
