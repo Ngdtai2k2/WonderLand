3cda4aeafaf0c0e1646ae3b1642ef9f4a0463e81
ngdtai2k2 <ngdtai2k2@gmail.com>
2024-03-17 17:07:59 +0700

    refactor and add route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
-import Home from './pages/home';
 import Login from './pages/login';
 import Profile from './pages/profile';
 import Register from './pages/register';
 import Settings from './pages/settings';
 import CreatePost from './pages/createPost';
+import TopPage from './pages/topPage';
+import TrendingPage from './pages/trendingPage';
+import FreshPage from './pages/freshPage';
+import AskPage from './pages/askPage';
+import HomePage from './pages/homePage';
 
 import NotFound from './components/NotFound';
 
@@ -16,13 +20,98 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<LayoutWithNavbar><Home /></LayoutWithNavbar>} />
-        <Route path="/profile/:id" element={<LayoutWithNavbar><Profile /></LayoutWithNavbar>} />
-        <Route path="/settings" element={<BlockedRoute><LayoutWithNavbar><Settings /></LayoutWithNavbar></BlockedRoute>} />
-        <Route path="/login" element={<BlockedRoute blocked="true"><Login /></BlockedRoute>} />
-        <Route path="/register" element={<BlockedRoute blocked="true"><Register /></BlockedRoute>} />
-        <Route path="/create/post" element={<BlockedRoute><LayoutWithNavbar><CreatePost /></LayoutWithNavbar></BlockedRoute>} />
-        <Route path="*" element={<NotFound />} />
+        <Route
+          path="/"
+          element={
+            <LayoutWithNavbar>
+              <HomePage />
+            </LayoutWithNavbar>
+          }
+        />
+        <Route
+          path="/profile/:id"
+          element={
+            <LayoutWithNavbar>
+              <Profile />
+            </LayoutWithNavbar>
+          }
+        />
+        <Route
+          path="/settings"
+          element={
+            <BlockedRoute>
+              <LayoutWithNavbar>
+                <Settings />
+              </LayoutWithNavbar>
+            </BlockedRoute>
+          }
+        />
+        <Route
+          path="/login"
+          element={
+            <BlockedRoute blocked="true">
+              <Login />
+            </BlockedRoute>
+          }
+        />
+        <Route
+          path="/register"
+          element={
+            <BlockedRoute blocked="true">
+              <Register />
+            </BlockedRoute>
+          }
+        />
+        <Route
+          path="/create/post"
+          element={
+            <BlockedRoute>
+              <LayoutWithNavbar>
+                <CreatePost />
+              </LayoutWithNavbar>
+            </BlockedRoute>
+          }
+        />
+        <Route
+          path="/top"
+          element={
+            <LayoutWithNavbar>
+              <TopPage />
+            </LayoutWithNavbar>
+          }
+        />
+        <Route
+          path="/trend"
+          element={
+            <LayoutWithNavbar>
+              <TrendingPage />
+            </LayoutWithNavbar>
+          }
+        />
+        <Route
+          path="/fresh"
+          element={
+            <LayoutWithNavbar>
+              <FreshPage />
+            </LayoutWithNavbar>
+          }
+        />
+        <Route
+          path="/ask"
+          element={
+            <LayoutWithNavbar>
+              <AskPage />
+            </LayoutWithNavbar>
+          }
+        />
+        <Route
+          path="*"
+          element={
+            <LayoutWithNavbar>
+              <NotFound />
+            </LayoutWithNavbar>
+          }
+        />
       </Routes>
     </Router>
   );
