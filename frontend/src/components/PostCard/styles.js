a9c5feece4fe69f6ae088097f1fac212869c640d
ngdtai2k2 <ngdtai2k2@gmail.com>
2024-03-22 08:33:54 +0700

    add CardStyled

--- frontend/src/components/PostCard/styles.js
+++ frontend/src/components/PostCard/styles.js
@@ -1,13 +1,20 @@
-import { Box, CardActions } from '@mui/material';
-import { styled } from '@mui/material/styles';
+import { Box, Card, CardActions } from "@mui/material";
+import { styled } from "@mui/material/styles";
 
 export const CardActionsStyled = styled(CardActions)(() => ({
-  display: 'flex',
-  justifyContent: 'space-between',
-  alignItems: 'center',
+  display: "flex",
+  justifyContent: "space-between",
+  alignItems: "center",
+}));
+
+export const CardStyled = styled(Card)(() => ({
+  border: "1px solid rgba(0,0,0,0.12)",
+  boxShadow: "none",
 }));
 
 export const BoxStyled = styled(Box)(() => ({
-  display: 'flex',
-  alignItems: 'center',
+  display: "flex",
+  alignItems: "center",
 }));
+
+
