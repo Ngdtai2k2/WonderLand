7c054c4ff59f5d7fa29e6a5edb8cfcd21fc6bae4
ngdtai2k2 <ngdtai2k2@gmail.com>
2024-03-22 09:40:51 +0700

    format code

--- frontend/src/components/PostCard/styles.js
+++ frontend/src/components/PostCard/styles.js
@@ -1,20 +1,19 @@
-import { Box, Card, CardActions } from "@mui/material";
-import { styled } from "@mui/material/styles";
+import { Box, Card, CardActions } from '@mui/material';
+import { styled } from '@mui/material/styles';
 
 export const CardActionsStyled = styled(CardActions)(() => ({
-  display: "flex",
-  justifyContent: "space-between",
-  alignItems: "center",
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center',
 }));
 
 export const CardStyled = styled(Card)(() => ({
-  border: "1px solid rgba(0,0,0,0.12)",
-  boxShadow: "none",
+  border: '1px solid rgba(0,0,0,0.12)',
+  boxShadow: 'none',
 }));
 
 export const BoxStyled = styled(Box)(() => ({
-  display: "flex",
-  alignItems: "center",
+  display: 'flex',
+  alignItems: 'center',
+  justifyContent: 'center',
 }));
-
-
