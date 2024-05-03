7ea87173cd470a7903e1eef9801c50d82e48920a
ngdtai2k2 <ngdtai2k2@gmail.com>
2024-05-03 10:18:36 +0700

    created readmore component

---
+++ frontend/src/components/Readmore/index.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react";
+
+export default function ReadMore({ children }) {
+  const [isReadMore, setIsReadMore] = useState(true);
+  const text = children;
+  const toggleReadMore = () => {
+    setIsReadMore(!isReadMore);
+  };
+  return (
+    <>
+      {isReadMore ? text.slice(0, 50) : text}
+      <span
+        onClick={toggleReadMore}
+        style={{ cursor: "pointer", fontStyle: "italic"}}
+      >
+        {isReadMore ? " ...read more" : " show less"}
+      </span>
+    </>
+  );
+}
