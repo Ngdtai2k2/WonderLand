2ffffb195d87ff983bee8f86337f7cc20ebd4901
ngdtai2k2 <ngdtai2k2@gmail.com>
2024-05-06 11:53:43 +0700

    refactor code

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import { CssBaseline } from '@mui/material';
 import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles';
 import { PersistGate } from 'redux-persist/integration/react';
 import { Provider } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
+
 import './index.css';
 import 'react-toastify/dist/ReactToastify.css';
 
