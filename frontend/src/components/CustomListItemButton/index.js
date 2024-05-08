f1938f87675779aa9ba8c7d4cf2f70d8ba19682d
ngdtai2k2 <ngdtai2k2@gmail.com>
2024-05-08 20:37:56 +0700

    show categories in drawer list

--- frontend/src/components/CustomListItemButton/index.js
+++ frontend/src/components/CustomListItemButton/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 
+import Avatar from '@mui/material/Avatar';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
@@ -10,11 +11,26 @@ export default function CustomListItemButton({
   onClick,
   icon,
   text,
+  category,
 }) {
   return (
     <ListItem disablePadding>
       <ListItemButton selected={selected} onClick={onClick}>
-        <ListItemIcon>{icon}</ListItemIcon>
+        {category ? (
+          <ListItemIcon sx={{ marginRight: -1 }}>
+            <Avatar
+              src={category.media.url}
+              variant="rounded"
+              alt={`${category.name}'s avatar`}
+              sx={{
+                width: 35,
+                height: 35,
+              }}
+            />
+          </ListItemIcon>
+        ) : (
+          <ListItemIcon sx={{ marginRight: -1 }}>{icon}</ListItemIcon>
+        )}
         <ListItemText primary={text} />
       </ListItemButton>
     </ListItem>
--- frontend/src/components/DrawerList/index.js
+++ frontend/src/components/DrawerList/index.js
@@ -1,17 +1,25 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
+import axios from 'axios';
 
 import Link from '@mui/material/Link';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
+import Typography from '@mui/material/Typography';
 
 import CustomListItemButton from '../CustomListItemButton';
 import { adminDrawerList, publicDrawerList } from '../../constants/drawerlist';
+import { BaseApi } from '../../constants/constant';
+import LoadingCircularIndeterminate from '../Loading';
 
 function DrawerList({ isAdmin }) {
   const location = useLocation();
-  const navigate = useNavigate();
+
+  const [categories, setCategories] = useState();
+  const [loading, setLoading] = useState(false);
   const [activeLink, setActiveLink] = useState(location.pathname);
+
+  const navigate = useNavigate();
 
   const handleLinkClick = (link) => {
     setActiveLink(link);
@@ -24,6 +32,32 @@ function DrawerList({ isAdmin }) {
 
   const itemsToMap = isAdmin ? adminDrawerList : publicDrawerList;
 
+  const getCategories = async () => {
+    try {
+      setLoading(true);
+      const response = await axios.get(`${BaseApi}/category`);
+      setCategories(response.data.result.docs);
+      localStorage.setItem(
+        'list-of-categories',
+        JSON.stringify(response.data.result.docs),
+      );
+    } catch (error) {
+      setCategories(null);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    const storedCategories = localStorage.getItem('list-of-categories');
+    if (storedCategories) {
+      setCategories(JSON.parse(storedCategories));
+    } else {
+      getCategories();
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+  
   return (
     <List>
       <ListItem>
@@ -40,6 +74,31 @@ function DrawerList({ isAdmin }) {
           text={item.text}
         />
       ))}
+      <Typography variant="body1" marginLeft={1} marginY={1}>
+        Discover
+      </Typography>
+      {loading ? (
+        <LoadingCircularIndeterminate />
+      ) : categories ? (
+        categories.map((category) => (
+          <CustomListItemButton
+            key={category._id}
+            selected={setSelectedLink(`/category/${category.name}`)}
+            onClick={() => handleLinkClick(`/category/${category.name}`)}
+            category={category}
+            text={category.name}
+          />
+        ))
+      ) : (
+        <Typography
+          variant="body2"
+          display="flex"
+          justifyContent="center"
+          alignItems="center"
+        >
+          No categories!
+        </Typography>
+      )}
     </List>
   );
 }
