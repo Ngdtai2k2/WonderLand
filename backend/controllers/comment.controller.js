94032db3a881acff38b28e7a85e680afb54e65b6
ngdtai2k2 <ngdtai2k2@gmail.com>
2024-05-11 15:10:58 +0700

    refactor code

--- backend/controllers/comment.controller.js
+++ backend/controllers/comment.controller.js
@@ -156,7 +156,10 @@ const commentController = {
         { new: true }
       ).populate(commentPopulateOptions);
 
-      return res.status(200).json({ message: "Comment has been created!", newComment: newComment.replies.pop() });
+      return res.status(200).json({
+        message: "Comment has been created!",
+        newComment: newComment.replies.pop(),
+      });
     } catch (error) {
       return res.status(500).json({ error });
     }
