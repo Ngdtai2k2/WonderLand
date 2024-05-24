adcdbc39740809a0aadb87dde95d9e831baded0f
ngdtai2k2 <ngdtai2k2@gmail.com>
2024-05-24 23:46:31 +0700

    enable react strict mode

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -24,20 +24,17 @@ console.log(
   'color: yellow; font-size: 30px',
 );
 root.render(
-  // <React.StrictMode>
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      <CssVarsProvider theme={themes}>
-        <CssBaseline />
-        <App />
-        <ToastContainer />
-      </CssVarsProvider>
-    </PersistGate>
-  </Provider>,
-  // </React.StrictMode>
+  <React.StrictMode>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <CssVarsProvider theme={themes}>
+          <CssBaseline />
+          <App />
+          <ToastContainer />
+        </CssVarsProvider>
+      </PersistGate>
+    </Provider>
+  </React.StrictMode>,
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
