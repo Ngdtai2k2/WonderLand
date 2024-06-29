40f1cbd628cd250962a4544b3c828e810bd2dfc5
nqocmei <ngdtai2k2@gmail.com>
2024-06-29 20:29:33 +0700

    refactor router

--- backend/index.js
+++ backend/index.js
@@ -8,22 +8,7 @@ const i18next = require("i18next");
 const i18nextBackend = require("i18next-fs-backend");
 const i18nextMiddleware = require("i18next-http-middleware");
 
-const authRoute = require("./routes/auth.route");
-const userRoute = require("./routes/user.route");
-const categoryRoute = require("./routes/category.route");
-const postRoute = require("./routes/post.route");
-const reactionRoute = require("./routes/reaction.route");
-const savePostRoute = require("./routes/savePost.route");
-const commentRoute = require("./routes/comment.route");
-const reportRoute = require("./routes/report.route");
-const notificationRoute = require("./routes/notification.route");
-const ruleRoute = require("./routes/rule.route");
-const searchRoute = require("./routes/search.route");
-const friendsRoute = require("./routes/friends.route");
-const socketRoute = require("./routes/socket.route");
-const chatRoute = require("./routes/chat.route");
-const messageRoute = require("./routes/message.route");
-const badWordRoute = require("./routes/badword.route");
+const routes = require("./routes");
 
 const socketService = require("./services/socket.service");
 
@@ -59,6 +44,7 @@ i18next.use(i18nextBackend).use(i18nextMiddleware.LanguageDetector)
     loadPath: "./locales/{{lng}}/translations.json"
   }
 })
+
 // cors
 const corsOptions = {
   origin: process.env.CORS_ORIGIN,
@@ -71,22 +57,8 @@ app.use(cookieParser());
 app.use(i18nextMiddleware.handle(i18next));
 app.use(express.json());
 
-app.use("/api/v1/auth", authRoute);
-app.use("/api/v1/user", userRoute);
-app.use("/api/v1/category", categoryRoute);
-app.use("/api/v1/post", postRoute);
-app.use("/api/v1/reaction", reactionRoute);
-app.use("/api/v1/save-post", savePostRoute);
-app.use("/api/v1/comment", commentRoute);
-app.use("/api/v1/notification", notificationRoute);
-app.use("/api/v1/report", reportRoute);
-app.use("/api/v1/rule", ruleRoute);
-app.use("/api/v1/search", searchRoute);
-app.use("/api/v1/friend", friendsRoute);
-app.use("/api/v1/socket", socketRoute);
-app.use("/api/v1/chat", chatRoute);
-app.use("/api/v1/message", messageRoute);
-app.use("/api/v1/bad-word", badWordRoute);
+// Use routes
+app.use("/api/v1", routes);
 
 io.on("connection", (socket) => {
   socketService.connection(socket);
---
+++ backend/routes/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const router = express.Router();
+
+const authRoute = require('./auth.route');
+const userRoute = require('./user.route');
+const categoryRoute = require('./category.route');
+const postRoute = require('./post.route');
+const reactionRoute = require('./reaction.route');
+const savePostRoute = require('./savePost.route');
+const commentRoute = require('./comment.route');
+const notificationRoute = require('./notification.route');
+const reportRoute = require('./report.route');
+const ruleRoute = require('./rule.route');
+const searchRoute = require('./search.route');
+const friendsRoute = require('./friends.route');
+const socketRoute = require('./socket.route');
+const chatRoute = require('./chat.route');
+const messageRoute = require('./message.route');
+const badWordRoute = require('./badword.route');
+
+// Define your routes here
+router.use('/auth', authRoute);
+router.use('/user', userRoute);
+router.use('/category', categoryRoute);
+router.use('/post', postRoute);
+router.use('/reaction', reactionRoute);
+router.use('/save-post', savePostRoute);
+router.use('/comment', commentRoute);
+router.use('/notification', notificationRoute);
+router.use('/report', reportRoute);
+router.use('/rule', ruleRoute);
+router.use('/search', searchRoute);
+router.use('/friend', friendsRoute);
+router.use('/socket', socketRoute);
+router.use('/chat', chatRoute);
+router.use('/message', messageRoute);
+router.use('/bad-word', badWordRoute);
+
+module.exports = router;
